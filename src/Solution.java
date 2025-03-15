1c7834d523fb1862cd4441e29febc0916a5b206c
Qwasez1337 <q629536@yandex.ru>
2025-03-15 19:51:37 +0400

    Check if array is good 2784

--- src/Solution.java
+++ src/Solution.java
@@ -1,36 +1,24 @@
 import java.util.*;
 
 class Solution {
-    public double largestTriangleArea(int[][] points) {
-        double maxArea = 0;
-        int n = points.length;
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums[nums.length - 1];
+        if (nums.length != n + 1) return false;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    int x1 = points[i][0];
-                    int y1 = points[i][1];
-                    int x2 = points[j][0];
-                    int y2 = points[j][1];
-                    int x3 = points[k][0];
-                    int y3 = points[k][1];
-
-                    double area = (double) Math.abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2;
-                    if (area > maxArea) {
-                        maxArea = area;
-                    }
-                }
-            }
+        for (int i = 1; i < nums.length - 1; i++) {
+            if(nums[i - 1] != i) return false;
         }
-        return maxArea;
+
+        return nums[nums.length - 1] == n && n == nums[nums.length - 2];
+
     }
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        System.out.println(solution.largestTriangleArea(new int[][]{new int[]{5,5},new int[]{0,1},new int[]{1,0}, new int[]{0,2}, new int[]{2,0}}));
+        Solution sol = new Solution();
+        System.out.println(sol.isGood(new int[]{2,2,2}));
     }
 }
-
 
 
 
